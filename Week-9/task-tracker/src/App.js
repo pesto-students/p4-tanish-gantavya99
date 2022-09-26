40c6affa26562c2dc9bbcd58ceaf4b7163a341d9
Gantavya Saraswat <gantavya99@gmail.com>
2022-09-26 10:28:52 +0530

    Added CSS to the file

--- Week-9/task-tracker/src/App.js
+++ Week-9/task-tracker/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import Header from "./Header";
+import Tasks from "./Tasks";
+import AddTask from './AddTask';
 function App() {
+  const [showAddTask,setShowAddTask]=useState(false); 
+ const [tasks, setTasks]=useState([
+  {
+    id:1,
+    text:`Thanks for using this Task Tracker!`,
+    day:`(This is a demo task!)`,
+    reminder:true,
+},
+
+ ])
+ const addTask=(task)=>{
+  const id=Math.floor(Math.random()*10000)+1;
+  const newTask={id, ...task}
+  setTasks([...tasks,newTask])
+ }
+      const deleteTask=(id)=>{
+        console.log('Task Deleted')
+        setTasks(tasks.filter((task)=>task.id!==id))
+      }
+      // const toggleReminder=(id)=>{
+      //   console.log(id);
+      // }
+      const toggleReminder=(id)=>{
+        setTasks(tasks.map((task)=>
+        task.id===id?
+        {...task,reminder:!task.reminder}:task))
+        console.log(`${id} is the ID`);
+      }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+     <Header onAdd={()=>setShowAddTask(!showAddTask)} 
+     showAdd={showAddTask}/>
+     {showAddTask && <AddTask onAdd={addTask}/>}
+     {tasks.length>0?<Tasks tasks ={tasks} 
+     onDelete={deleteTask} onToggle={toggleReminder}/>:'No tasks Added'}
+     
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ Week-9/task-tracker/src/Task.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+const Task = ({ task, onDelete,onToggle }) => {
+    return (
+        <div className={`task ${task.reminder?'reminder' : ''}`} 
+        onDoubleClick={()=>onToggle(task.id)}>
+            {task.text} <FaTimes className="X-icon" 
+            onClick={()=>onDelete(task.id)} />
+            <div className="day">{task.day}</div>
+        </div>
+    );
+};
+
+ export default Task
+// import React from 'react'
+// import { FaTimes } from 'react-icons/fa'
+
+// const Task = ({ task, onDelete, onToggle }) => {
+//   return (
+//     <div
+//       className={`task ${task.reminder && 'reminder'}`}
+//       onDoubleClick={() => onToggle(task.id)}
+//     >
+//       <h3>
+//         {task.text}{' '}
+//         <FaTimes
+//           style={{ color: 'red', cursor: 'pointer' }}
+//           onClick={() => onDelete(task.id)}
+//         />
+//       </h3>
+//       <p>{task.day}</p>
+//     </div>
+//   )
+// }
+
+// export default Task
